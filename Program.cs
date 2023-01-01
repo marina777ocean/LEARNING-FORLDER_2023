6c53618ec5583a4c59b62f834376ced0bcd57999
Marina <vip007ad@gmail.com>
2023-01-01 11:51:21 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Демонстрация решения
+//Напишите программу, которая на вход принимает число и выдает 
+//его квадрат(число, умноженное * само на себя)
+
+int number = int.Parse(Console.ReadLine());
+
+int quarter = number * number;
+
+Console.WriteLine(quarter);
+
+
