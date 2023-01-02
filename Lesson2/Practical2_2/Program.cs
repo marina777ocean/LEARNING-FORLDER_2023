75f40bef1d844b6562ef9547fa61f352cd4c662f
Marina <vip007ad@gmail.com>
2023-01-02 08:30:26 -0800

    Add files via upload

---
+++ Lesson2/Practical2_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 2.Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4 16, 4 -> кратно
+using System;
+
+namespace MultipleChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get input from the user
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            // Check if num2 is a multiple of num1
+            if (num2 % num1 == 0)
+            {
+                Console.WriteLine("The second number is a multiple of the first.");
+            }
+            else
+            {
+                Console.WriteLine("The second number is not a multiple of the first. Reminder: " + (num2 % num1));
+            }
+        }
+    }
+}
---
+++ Lesson2/Program.cs
@@ -0,0 +1,22 @@
+﻿// 1. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46 782 -> 72 918 -> 98
+
+
+Random random = new Random();
+int number = random.Next(10,100);
+Console.WriteLine(number);
+
+int FirstNumber = number / 10;
+
+int LastNumber = number % 10;
+
+if (FirstNumber > LastNumber)
+{
+    Console.WriteLine(FirstNumber);
+}
+else
+{
+    Console.WriteLine(LastNumber);
+}
+
+
